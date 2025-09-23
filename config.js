a927d52e7d218b14aa7a34eec640017d52c91013
jeganchinna <140259566+jeganchinna@users.noreply.github.com>
2025-09-23 08:54:42 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,3 @@
+// Example config file (Supabase or other API settings)
+export const SUPABASE_URL = "hhttps://fdkxhivfbrbhelgvobhz.supabase.co";
+export const SUPABASE_KEY ="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZka3hoaXZmYnJiaGVsZ3ZvYmh6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg1NDc4MjIsImV4cCI6MjA3NDEyMzgyMn0.iY6jP7kXBPLYawajUyGwiXOvU5_QKfC-DCzVzWeTH5E";
